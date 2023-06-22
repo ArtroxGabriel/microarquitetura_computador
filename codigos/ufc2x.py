54e26f19c77951c50862c844152ad3c7b61c8247
Gabrigas <78170301+ArtroxGabriel@users.noreply.github.com>
2023-06-22 11:43:58 +0000

    baiano

--- codigos/ufc2x.py
+++ codigos/ufc2x.py
@@ -2,12 +2,12 @@ import memory
 from array import array
 
 # REGISTRADORES
-MPC = 0 
-MIR = 0     
+MPC = 0
+MIR = 0
 
 MAR = 0
 MDR = 0
-PC = 0 
+PC = 0
 MBR = 0
 X = 0
 Y = 0
@@ -30,14 +30,39 @@ def read_regs(reg_num):
     BAR_A = H
 
     if reg_num == 0:
-        BUS_B = MDR
+        BAR_B = MDR
     elif reg_num == 1:
-        BUS_B = PC
+        BAR_B = PC
     elif reg_num == 2:
-        BUS_B = MBR
+        BAR_B = MBR
     elif reg_num == 3:
-        BUS_B = X
+        BAR_B = X
     elif reg_num == 4:
-        BUS_B = Y
+        BAR_B = Y
     else:
-        BUS_B = 0
+        BAR_B = 0
+
+def write_regs(reg_bits):
+    global MAR, MDR, PC, X, Y, H, BAR_C
+
+    if reg_bits & 0b100000:
+        MAR = BAR_C
+    if reg_bits & 0b010000:
+        MDR = BAR_C
+    if reg_bits & 0b001000:
+        PC = BAR_C
+    if reg_bits & 0b000100:
+        X = BAR_C
+    if reg_bits & 0b000010:
+        Y = BAR_C
+    if reg_bits & 0b000001:
+        H = BAR_C
+
+def ula(bits_de_controle):
+    global N, Z, BAR_A, BAR_B, BAR_C
+    
+    a = BAR_A
+    B = BAR_C
+    saida = 0
+    
+    
