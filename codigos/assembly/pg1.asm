ecb859dd45229db3d31c84f255789a9935dd5fc5
ArtroxGabriel <artroxgabriel@gmail.com>
2023-06-24 12:13:24 -0300

    no ideia

--- codigos/assembly/pg1.asm
+++ codigos/assembly/pg1.asm
@@ -5,7 +5,7 @@ a ww 0 # guardar na word 1 sempre
 b ww 4
 c ww 6
 
-main    add x, b
-        add x, c
-        mov x, a
-        halt
+main    add x, b # X <- X + B
+        add x, c # X <- X + C
+        mov x, a # A <- X
+        halt     # finaliza
