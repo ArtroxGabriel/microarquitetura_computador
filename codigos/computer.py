2c3f221880219c51e0ff6f153f3c05098dba445c
ArtroxGabriel <artroxgabriel@gmail.com>
2023-06-24 11:47:16 -0300

    testando o reg Y

--- codigos/computer.py
+++ codigos/computer.py
@@ -1,8 +1,22 @@
 import ufc2x as cpu
 import memory as mem
 import clock as clk
- 
+
+mem.write_word(2, 100)
+mem.write_word(3, 50)
+
+# y = y + mem[2]
+mem.write_byte(1, 17)
+mem.write_byte(2, 2)
+
+# y = y + mem[3]
+mem.write_byte(3, 17)
+mem.write_byte(4, 3)
+
+# mem[1] = y
+mem.write_byte(5, 25)
+mem.write_byte(6, 1)
 
 clk.start([cpu])
 
-print(mem.read_word(150))
+print(mem.read_word(1))
